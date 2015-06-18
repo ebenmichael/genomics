07dbc4a94cd8431aad40c735268b298995f85530
Cynthia <cynthiasschen@gmail.com>
2015-06-17 22:12:01 -0400

    got array dimensions

--- readL.java
+++ readL.java
@@ -30,48 +30,46 @@ public class readL {
   BufferedReader reader1 = new BufferedReader(new FileReader(args[0]));
   BufferedReader reader2 = new BufferedReader(new FileReader(args[1]));
   
-  ArrayList<Base> bases1 = new ArrayList<Base>();
-  ArrayList<Base> bases2 = new ArrayList<Base>();
-  bases1.ensureCapacity(1750000000);
-  bases2.ensureCapacity(1750000000);
+  Base[] bases1 = new Base[1680373120];
+  Base[] bases2 = new Base[1421431552];
   
   System.out.println("Reading Chromosomes 1-9");
  
   String current1;
   int count = 0;
   while((current1 = reader1.readLine()) != null){ 
-    System.out.println(count);
-    count++;
-    if(count % 10000 == 0){
-      System.out.println(count + " lines read");
+    if(count % 100000000 == 0){
+      System.out.println(count + " bases read");
     }
-   try{
-     for(int i = 0; i < current1.length(); i++){
-       char c1 = current1.charAt(i);
-       bases1.add(new Base(c1));
-       counts1.put(c1, counts1.get(c1) + 1);
-     }
+    try{
+      for(int i = 0; i < current1.length(); i++){
+        char c1 = current1.charAt(i);
+        bases1[count] = new Base(c1);
+        counts1.put(c1, counts1.get(c1) + 1);
+        count++;
+      }
    }
    catch(IllegalArgumentException e){
      System.out.println(current1);
    }
   }
 
-  System.out.println("Reading Chromosomes 10-");
+  System.out.println("Reading Chromosomes 10-end");
   
   reader1.close();
   String current2;
+  int count2 = 0;
   while((current2 = reader2.readLine()) != null){ 
-    if(count % 1000 == 0){
-      System.out.println(count + " lines read");
-      count++;
+    if(count % 100000000 == 0){
+      System.out.println(count + " bases read");
     }
    try{
-     for(int i = 0; i < current2.length(); i++){
-       char c2 = current2.charAt(i);
-       bases2.add(new Base(c2));
-       counts2.put(c2, counts2.get(c2) + 1);
-     }
+      for(int i = 0; i < current2.length(); i++){
+        char c2 = current2.charAt(i);
+        bases1[count2] = new Base(c2);
+        counts1.put(c2, counts1.get(c2) + 1);
+        count2++;
+      }
    }
    catch(IllegalArgumentException e){
      System.out.println(current2);
@@ -93,9 +91,7 @@ public class readL {
   countArray2[1] = counts2.get('C');
   countArray2[2] = counts2.get('G');
   countArray2[3] = counts2.get('T');
-  System.out.println("Casting ArrayList<Base>s to Base[]");
-  Base[] baseArray1 = (Base[]) bases1.toArray();
-  Base[] baseArray2 = (Base[]) bases2.toArray();
+  
   System.out.println("Creating FMIndex 1 for Chromosomes 1-9");
   FMIndex2 fm1 = new FMIndex2(baseArray1, countArray1, Integer.parseInt(args[4]));
   System.out.println("Writing FMIndex 1 to disk");
@@ -114,5 +110,4 @@ public class readL {
   oos2.writeObject(fm2);
   oos2.close();
  }
- 
 }
