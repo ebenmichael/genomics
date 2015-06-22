4ea78ecd64deefe1d9ea364b076682a3511f6886
Eli Ben-Michael <eeb2142@columbia.edu>
2015-06-22 15:06:27 -0400

    Bigger font

--- presentation/figures.py
+++ presentation/figures.py
@@ -11,7 +11,7 @@ def plotVals(csvFile,xlab,ylab,title,fName = None,save = False):
     dat = pd.read_csv(csvFile)
     x = dat.iloc[:,0]
     y = dat.iloc[:,1]
-    sns.set_context('poster')
+    sns.set_context('poster',font_scale=1.5)
     sns.set_style("whitegrid",{"axes.facecolor":"#EEECE1","figure.facecolor":"#EEECE1"
                 ,"xtick.color":".2","ytick.color":".2",
                 "text.color":".1","axes.labelcolor":".1"})
