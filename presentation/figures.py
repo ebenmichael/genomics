8e360c3587f4bad819b90013809cc0e6ff821363
Eli Ben-Michael <eeb2142@columbia.edu>
2015-06-22 14:58:12 -0400

    Changed aspect ratio

--- presentation/figures.py
+++ presentation/figures.py
@@ -11,18 +11,18 @@ def plotVals(csvFile,xlab,ylab,title,fName = None,save = False):
     dat = pd.read_csv(csvFile)
     x = dat.iloc[:,0]
     y = dat.iloc[:,1]
-    
+    sns.set_context('poster')
     sns.set_style("whitegrid",{"axes.facecolor":"#EEECE1","figure.facecolor":"#EEECE1"
                 ,"xtick.color":".2","ytick.color":".2",
                 "text.color":".1","axes.labelcolor":".1"})
-                
+    plt.figure(figsize=(12, 16))      
     plt.plot(x,y,color = '#00274c')
     plt.xlabel(xlab)
     plt.ylabel(ylab)
     plt.title(title)
     
     if save:
-        plt.savefig(fName, transparent = True)
+        plt.savefig(fName, transparent = True, dpi = 400)
                 
     
 
