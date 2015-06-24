d31bcd6f1555ceb6b703bd2579d9828f8ba5dea1
Eli Ben-Michael <eeb2142@columbia.edu>
2015-06-24 15:20:15 -0400

    New figure

--- presentation/figures.py
+++ presentation/figures.py
@@ -7,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plotVals(csvFile,xlab,ylab,title,fName = None,save = False):
+def plotVals(csvFile,xlab,ylab,title,fName = None,save = False, log = False):
     dat = pd.read_csv(csvFile)
     x = dat.iloc[:,0]
     y = dat.iloc[:,1]
@@ -20,6 +20,7 @@ def plotVals(csvFile,xlab,ylab,title,fName = None,save = False):
     plt.xlabel(xlab)
     plt.ylabel(ylab)
     plt.title(title)
+
     
     if save:
         plt.savefig(fName, transparent = True, dpi = 200)
@@ -34,9 +35,9 @@ sns.set_style("whitegrid",{"axes.facecolor":"#EEECE1","figure.facecolor":"#EEECE
             "text.color":".1","axes.labelcolor":".1"})
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(dat.iloc[:,0],dat.iloc[:,1],label = 'AC')
-ax.plot(dat.iloc[:,0],dat.iloc[:,2], label = 'CGG')
-ax.plot(dat.iloc[:,0],dat.iloc[:,3], label = 'TTAGGG')
+ax.plot(dat.iloc[:,0],dat.iloc[:,1],color = '#002d62',label = 'AC')
+ax.plot(dat.iloc[:,0],dat.iloc[:,2], color = '#9D0E00', label = 'CGG')
+ax.plot(dat.iloc[:,0],dat.iloc[:,3], color = '#ffcb05', label = 'TTAGGG')
 ax.set_yscale('log')
 leg = ax.legend(title = 'STR')
 plt.xlabel('Number of STR Repeats')
